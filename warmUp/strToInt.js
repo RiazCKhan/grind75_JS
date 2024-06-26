7589100a37f1ef9b390e8b3b6d0babc0d2c3c178
RiazK <RiazCKhan@gmail.com>
2024-06-26 16:05:13 -0400

    solution accounts for 32 bit. Failing new test case

--- warmUp/strToInt.js
+++ warmUp/strToInt.js
@@ -1,5 +1,7 @@
+/*
+The below solution fails the current testcase
+*/
 const myAtoi = (str) => {
-
   let result = [];
   let trimStr = str.trimStart()
 
@@ -17,10 +19,20 @@ const myAtoi = (str) => {
     }
   }
 
+  let final = parseInt(result.join(''))
+
+  if (final < -(2 ** 31)) {
+    return -(2 ** 31)
+  } else if (final > (2 ** 31) - 1) {
+    return (2 ** 31) - 1
+  }
+
   if (result.length === 1 && result[0] === '-') return 0
   if (result.length === 0) return 0
-  return parseInt(result.join(''))
+  return final
 }
 
-let testCase = "king 25"
+let testCase = "+-12"
+// output -12
+// expected 0
 console.log(typeof (myAtoi(testCase)))
