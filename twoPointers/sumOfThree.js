45099367797c3b32483c40862ad45e9c28861080
RiazK <RiazCKhan@gmail.com>
2024-06-30 16:51:33 -0400

    initial commit with psuedo code

---
+++ twoPointers/sumOfThree.js
@@ -0,0 +1,22 @@
+// Using Two Pointer + Current Pointer\
+// Sort Array in ascending order
+// Create 3 variables, Low, Current, High
+// IF 3 Values are > target, decrement High
+// IF 3 Values are < target, increment Low
+// Check if Target Found Return True
+// ELSE Increment Current
+// Return False
+
+
+const sumOfThree = (nums, target) => {
+
+  let sortedNums = nums.sort();
+
+
+
+}
+
+
+let testcase = [5, 1, 2, 9, 3]; // 1, 2, 3, 5, 9
+
+console.log(sumOfThree(testcase));
