7f8d875015f47f409050ee65a5f283763f9fefa5
RiazK <RiazCKhan@gmail.com>
2024-06-30 07:29:44 -0400

    add psuedo code

---
+++ twoPointers/sortColors.js
@@ -0,0 +1,10 @@
+// Use the Two Pointers Pattern + 1 Pointer
+// Start, Current, End
+// WHILE Current <= End
+// IF Current is 0; swap with Start
+// -- Increment Cur and Start
+
+// ELSE Current is 1, increment Current
+
+// ELSE Current is 2; swap with End
+// -- decrement End
