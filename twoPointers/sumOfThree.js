3e950b507f24967c873de7cbeddb6938290d46d4
RiazK <RiazCKhan@gmail.com>
2024-06-30 17:26:33 -0400

    update psuedo code

--- twoPointers/sumOfThree.js
+++ twoPointers/sumOfThree.js
@@ -1,6 +1,7 @@
 // Using Two Pointer + Current Pointer\
 // Sort Array in ascending order
 // Create 3 variables, Low, Current, High
+// Current is Represented by the Outer Nested Loop Idex
 // IF 3 Values are > target, decrement High
 // IF 3 Values are < target, increment Low
 // Check if Target Found Return True
@@ -10,13 +11,9 @@
 
 const sumOfThree = (nums, target) => {
 
-  let sortedNums = nums.sort();
-
-
-
 }
 
 
-let testcase = [5, 1, 2, 9, 3]; // 1, 2, 3, 5, 9
+let testcase = []
 
 console.log(sumOfThree(testcase));
