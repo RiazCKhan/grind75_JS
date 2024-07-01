68070cd8d06172f7ab22a325334bbbb80da76c4b
RiazK <RiazCKhan@gmail.com>
2024-07-01 08:32:12 -0400

    partial solution: does not account for test case 0-1, output -1, expected 0

--- warmUp/myAtoi.js
+++ warmUp/myAtoi.js
@@ -12,4 +12,37 @@
 
 function myAtoi(s) {
 
+  let string = s.trimStart();
+
+  let sign = 1;
+  let result = 0;
+
+  for (let i = 0; i < string.length; i++) {
+
+    if (string[i] === '-') {
+
+      sign = -1;
+
+    } else if (string[i] >= '0' && string[i] <= '9') {
+
+      const digit = string[i].charCodeAt(0) - '0'.charCodeAt(0);
+
+      result = result * 10 + digit
+
+    }
+    // else if (result > (2 ** 31) || result < -(2 ** 31)) {
+
+    // return sign === 1 ? (2 ** 31) - 1 : -(2 ** 31)
+
+    // } 
+    else if (isNaN(parseInt(string[i]))) {
+      break;
+
+    }
+  }
+  return result * sign;
 }
+
+let testcase = '0-1';
+
+// console.log(myAtoi(testcase))
