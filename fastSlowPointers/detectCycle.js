2f9931104b9e96c79a48abd3a6d7a24d69c1f4ff
RiazK <RiazCKhan@gmail.com>
2024-07-15 14:43:33 -0400

    initial commit with first problem detect cycle in a linked list

---
+++ fastSlowPointers/detectCycle.js
@@ -0,0 +1,10 @@
+// Initialize both fast and slow pointers to the head of the linked list
+// Move the slow pointer one node ahead and the fast pointer two nodes ahead
+// Check if both pointers point to the same node -- return True
+// Else if the fast pointer reaches the end of the linked list return False
+
+
+export function detectCycle(head) {
+
+
+}
