37fbae6609d1728817adc12dce878164971d169e
RiazK <RiazCKhan@gmail.com>
2024-07-17 09:18:17 -0400

    add helper function to determine bad version

--- modBinarySearch/firstBadVersion.js
+++ modBinarySearch/firstBadVersion.js
@@ -1,1 +1,9 @@
 // Initial commit
+
+
+const isBadVersion = (version, badVersionList) => {
+  for (let i = 0; i < badVersionList.length - 1; i++) {
+    if (badVersionList[i] === version) return true;
+  }
+  return false;
+}
