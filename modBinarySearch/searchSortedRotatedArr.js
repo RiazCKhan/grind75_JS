70663b9a8e34a89e452803bc90c3ae32c1a5d09f
RiazK <RiazCKhan@gmail.com>
2024-07-19 09:46:32 -0400

    add half solution with comments and psuedo code

--- modBinarySearch/searchSortedRotatedArr.js
+++ modBinarySearch/searchSortedRotatedArr.js
@@ -1,24 +1,45 @@
-
+// Write basic binary search -> modify based on rotate condition
+// Check if the first half of the array is sorted
+// Check if the target is within the first half of the sorted array
+// If both conditions are true, increment and decrement start/end until target is found
+// ELSE perform the same steps for the second hald of the array
 
 
 const binarySearchRotated = (nums, target) => {
 
-  let low = 0
-  let high = nums.length - 1
+  let start = 0
+  let end = nums.length - 1
 
-  while (low <= high) {
-    let mid = Math.floor((low + high) / 2)
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2)
 
     if (nums[mid] === target) {
       return mid
-    } else if (nums[mid] < target) {
-      low = mid + 1
-    } else {
-      high = mid - 1
     }
-  }
 
-  return -1;
+    // The first half of the array is sorted
+    if (nums[mid] >= nums[start]) {
+      // If the target falls within the range of the first sorted half of the array
+      if (nums[start] >= target && target <= nums[start]) {
+        start = mid + 1
+      } else {
+        end = mid - 1
+      }
+    }
+
+    // The second half of the array is sorted.
+    else if (nums[mid] <= nums[end]) {
+
+      if (nums[mid] > nums[end]) {
+        start = mid + 1
+      } else {
+        end = mid - 1
+      }
+    }
+
+  }
+  return -1
 }
+
 
 console.log(binarySearchRotated([4, 5, 6, 1, 2, 3], 3))
