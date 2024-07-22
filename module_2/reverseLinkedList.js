bea35afdfb7ad01d48b8cb8515ac4e88f30d7225
RiazK <RiazCKhan@gmail.com>
2024-07-22 13:31:46 -0400

    save partial incomplete solution

--- module_2/reverseLinkedList.js
+++ module_2/reverseLinkedList.js
@@ -1,4 +1,33 @@
+// Initial 3 Pointers, Prev, Cur, Next
+
+
+
 const reverse = (head) => {
 
+  let prev = null;
+  let cur = head;
+  let next = cur;
 
+  while (cur !== null) {
+
+    // save the node ahead of cur
+    next = cur.next;
+
+    // assign head pointer to tail null or next node
+    cur.next = prev;
+
+    // move prev to current
+    prev = cur;
+
+    // move cur to next (cannot use cur because 'link' was reassigned)
+    cur = next;
+
+    // increment current for next iteration
+    cur = cur.next;
+
+
+  }
+  head = prev;
+
+  return head;
 }
