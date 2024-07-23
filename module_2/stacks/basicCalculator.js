939596908dfa312f28bfb0872f68d8b3a1cfc980
RiazK <RiazCKhan@gmail.com>
2024-07-23 15:39:50 -0400

    initial commit with some puesdo code

---
+++ module_2/stacks/basicCalculator.js
@@ -0,0 +1,23 @@
+// Order of Operation
+// 1. Convert consecutive digits into a single number
+// 2. Handle +/- operators
+// 3. Handle "("
+// 4. Handle ")"
+
+
+const calculator = (expression) => {
+  // expression is a string
+
+  let stack = [];
+
+  for (let i = 0; i < expression.length; i++) {
+
+    // Check if number
+
+  }
+
+}
+
+
+let testcase = '(13 + 50) + (56 - 29 - (7 + 2))'
+calculator(testcase);
