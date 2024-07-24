64efa2f777c9d2329b2046bbffb2a2197fbb56d9
RiazK <RiazCKhan@gmail.com>
2024-07-24 09:36:14 -0400

    update conditional to identify number

--- module_2/stacks/basicCalculator.js
+++ module_2/stacks/basicCalculator.js
@@ -9,15 +9,39 @@ const calculator = (expression) => {
   // expression is a string
 
   let stack = [];
+  let currentNum = 0;
+  let sign = 0;
+  let result = 0;
 
   for (let i = 0; i < expression.length; i++) {
 
-    // Check if number
+    // Identify Number
+    if (expression[i] >= '0' && expression[i] <= '9') {
+
+
+
+      // currentNum += parseInt(expression[i])
+    }
+
+    //   if (expression[i] === '+') {
+    //     sign = 1
+    //     result += currentNum * sign
+    //   } else if (expression[i] === '-') {
+    //     sign = -1
+    //     result += currentNum * sign
+    //   }
+
+    //   if (expression[i] === '(') {
+    //     stack.push(result)
+    //     stack.push(sign)
+    //   } else if (expression[i] === ')') {
+    //     result += stack.pop()
+    //   }
 
   }
-
+  // return result
 }
 
 
-let testcase = '(13 + 50) + (56 - 29 - (7 + 2))'
-calculator(testcase);
+let testcase = '13 + (1 + 2) - 3'
+console.log(calculator(testcase));
