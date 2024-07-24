3ca00120a0e297e568fb17d3a3781811ab85e010
RiazK <RiazCKhan@gmail.com>
2024-07-24 11:51:52 -0400

    initial commit reverse polish notation

---
+++ module_2/stacks/rpn.js
@@ -0,0 +1,13 @@
+const rpn = (tokens) => {
+  let stack = [];
+  let signVal = 0
+  let result = 0
+
+  for (let i = 0; i < tokens.length; i++) {
+
+
+  }
+}
+
+let testcase = ['8', '2', '/', '3', '+'];
+console.log(rpn(testcase))
