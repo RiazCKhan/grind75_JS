35a11c4babd061121eb6a46c52fd147db3802bb3
RiazK <RiazCKhan@gmail.com>
2024-07-25 14:19:22 -0400

    initial commit

---
+++ module_2/stacks/myQueue.js
@@ -0,0 +1,25 @@
+class MyQueue {
+  constructor() {
+    this.stack1 = new Array();
+    this.stack2 = new Array();
+  }
+
+  push(val) {
+  }
+
+  pop() {
+    // returns and removes value at top 
+  }
+
+  peek() {
+    // return value at top
+  }
+
+  empty() {
+    // return true/false 
+  }
+}
+
+let queue = new MyQueue(2)
+
+console.log(queue)
