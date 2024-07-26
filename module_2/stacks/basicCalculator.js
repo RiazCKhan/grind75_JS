83b6e6f40cb328ea14389e5fcedd5dc341266d6d
RiazK <RiazCKhan@gmail.com>
2024-07-26 09:59:19 -0400

    add logic to handle addition and subtraction

--- module_2/stacks/basicCalculator.js
+++ module_2/stacks/basicCalculator.js
@@ -9,20 +9,38 @@ const calculator = (expression) => {
   // expression is a string
   let stack = [];
   let number = 0;
-  let signValue = 1;
+  let signValue = 0;
   let result = 0;
 
   for (let i = 0; i < expression.length; i++) {
     // Identify Number
+    if (expression[i] >= '0' && expression[i] <= '9') {
+      number = number * 10 + parseInt(expression[i]);
+      stack.push(number);
+    }
 
     // Identify +/- operator
+    if (expression[i] === '+') {
+      signValue = 1;
+      let num1 = stack.pop()
+      let num2 = stack.pop()
+      result += num1 + (num2 * signValue)
+      number = 0
+    } else if (expression[i] === '-') {
+      signValue = -1;
+      let num1 = stack.pop()
+      let num2 = stack.pop()
+      result += num1 + (num2 * signValue)
+      number = 0
+    }
 
     // Identify bracket operator
 
   }
+  return result
 }
 
 
-let testcase = '13 + (1 + 2) - 3'
-// let testcase = '13'
+// let testcase = '13 + (1 + 2) - 3'
+let testcase = '13 - 1'
 console.log(calculator(testcase));
