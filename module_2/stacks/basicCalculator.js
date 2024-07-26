c21c5bb6aaf40054510e3545723aad3666eae190
RiazK <RiazCKhan@gmail.com>
2024-07-26 09:43:28 -0400

    add function variables

--- module_2/stacks/basicCalculator.js
+++ module_2/stacks/basicCalculator.js
@@ -4,45 +4,22 @@
 // 3. Handle "("
 // 4. Handle ")"
 
-// Identify consecutive Numbers 
-
-
 
 const calculator = (expression) => {
   // expression is a string
-
   let stack = [];
   let number = 0;
-  let sign = 0;
+  let signValue = 1;
   let result = 0;
 
   for (let i = 0; i < expression.length; i++) {
-
     // Identify Number
-    if (expression[i] >= '0' && expression[i] <= '9') {
-      number = number * 10 + parseInt(expression[i])
-    }
 
     // Identify +/- operator
-    if (expression[i] === '+') {
-      sign = 1
-      result = result + number * sign
-      number = 0
-    } else if (expression[i] === '-') {
-      sign = -1
-      result = result + number * sign
-      number = 0
-    }
 
     // Identify bracket operator
-    if (expression[i] === '(') {
-      console.log(`Expression[i] ${expression[i]} && Identify ( ${sign}`)
-    } else if (expression[i] === ')') {
-      console.log(`Expression[i] ${expression[i]} && Identify ) ${sign}`)
-    }
 
   }
-  return null
 }
 
 
