e83f406dcd5e73e056c5f3fc69a9f48da72534e6
RiazK <RiazCKhan@gmail.com>
2024-07-29 16:06:09 -0400

    add puesdocode for push method

--- module_2/stacks/myQueue.js
+++ module_2/stacks/myQueue.js
@@ -1,4 +1,7 @@
-// INCOMPLETE
+// 1. Transfer the existing elements from Stack1 to Stack2
+// Push the new element in Stack1
+// Transfer the elements back to Stack1
+
 class MyQueue {
   constructor() {
     this.stack1 = new Array();
@@ -9,6 +12,7 @@ class MyQueue {
     while (!this.stack1.isEmpty()) {
       this.stack2.push(this.stack1.pop())
     }
+    // Why push onto Stack1?
     this.stack1.push(x)
     while (!this.stack2.isEmpty()) {
       this.stack1.push(this.stack2.pop())
