6f7c681cc9219d2c89506349a46b499b24133337
RiazK <RiazCKhan@gmail.com>
2024-07-29 14:04:31 -0400

    update: signvalue initialize as 1, not 0; return the result + the remaining operand to be evaluated

--- module_2/stacks/basicCalculator.js
+++ module_2/stacks/basicCalculator.js
@@ -5,42 +5,43 @@
 // 4. Handle ")"
 
 
-const calculator = (expression) => {
+function calculator(expression) {
   // expression is a string
   let stack = [];
   let number = 0;
-  let signValue = 0;
+  let signValue = 1;
   let result = 0;
 
   for (let i = 0; i < expression.length; i++) {
     // Identify Number
     if (expression[i] >= '0' && expression[i] <= '9') {
       number = number * 10 + parseInt(expression[i]);
-      stack.push(number);
     }
 
     // Identify +/- operator
-    if (expression[i] === '+') {
-      signValue = 1;
-      let num1 = stack.pop()
-      let num2 = stack.pop()
-      result += num1 + (num2 * signValue)
-      number = 0
+    else if (expression[i] === '+') {
+      // result += number * signVal is the evaluation of the 'left' expression
+      // The 'left' expression is only evaluated when a new operator is found
+      result += number * signValue;
+      signValue = 1
+      number = 0;
     } else if (expression[i] === '-') {
-      signValue = -1;
-      let num1 = stack.pop()
-      let num2 = stack.pop()
-      result += num1 + (num2 * signValue)
+      // result += number * signVal is the evaluation of the 'left' expression
+      // The 'left' expression is only evaluated when a new operator is found
+      result += number * signValue
+      signValue = -1
       number = 0
     }
 
     // Identify bracket operator
 
   }
-  return result
+  return result + number * signValue;
 }
 
 
 // let testcase = '13 + (1 + 2) - 3'
-let testcase = '13 - 1'
-console.log(calculator(testcase));
+// let testcase = '(1 + 2)'
+let testcase = '13 + 1 - 3 - 1'
+calculator(testcase);
+// console.log(calculator(testcase));
