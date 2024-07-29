308551836e9ed326e1d6543994bb2e207780d870
RiazK <RiazCKhan@gmail.com>
2024-07-29 16:02:02 -0400

    investigate the relationship between 2 stacks in the push method

--- module_2/stacks/myQueue.js
+++ module_2/stacks/myQueue.js
@@ -1,22 +1,30 @@
+// INCOMPLETE
 class MyQueue {
   constructor() {
     this.stack1 = new Array();
     this.stack2 = new Array();
   }
 
-  push(val) {
+  push(x) {
+    while (!this.stack1.isEmpty()) {
+      this.stack2.push(this.stack1.pop())
+    }
+    this.stack1.push(x)
+    while (!this.stack2.isEmpty()) {
+      this.stack1.push(this.stack2.pop())
+    }
   }
 
   pop() {
-    // returns and removes value at top 
+    return this.stack1.pop()
   }
 
   peek() {
-    // return value at top
+    return this.stack1.top()
   }
 
   empty() {
-    // return true/false 
+    return this.stack1.isEmpty()
   }
 }
 
